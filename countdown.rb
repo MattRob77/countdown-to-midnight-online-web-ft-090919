1247693ae9aa00108ceeb2d56f8e2786a6baabd5
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-10 16:13:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,10 @@
 #write your code here
 
 def countdown(number)
-  while > 0
-  puts "#{number} SECOND(S)!"
-  end 
-  return "HAPPY NEW YEAR!"
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+

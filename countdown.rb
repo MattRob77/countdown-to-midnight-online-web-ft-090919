4efea98d4ca3a05c2a9d59adc2d3283815c4468b
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-10 16:16:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,13 +9,12 @@ def countdown(number)
 end
 
 
-def countdown_with_sleep(nunmber)
-  while number > 0 
-   puts "#{number} SECOND(S)!"
-   number -= 1
-   sleep(1)
- end 
- return "HAPPY NEW YEAR!"
-end 
-  
+def countdown_with_sleep(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        sleep(1)
+    end
+    return "HAPPY NEW YEAR!"
+end
 
